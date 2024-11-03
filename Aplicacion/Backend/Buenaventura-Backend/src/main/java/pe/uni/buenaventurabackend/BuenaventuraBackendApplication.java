1c1aefffc59fe5f959c2a32447fa9f37b641e906
Adal1475 <adalito.14@gmail.com>
2024-11-03 13:14:56 -0500

    Estructura backend

---
+++ Aplicacion/Backend/Buenaventura-Backend/src/main/java/pe/uni/buenaventurabackend/BuenaventuraBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.uni.buenaventurabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuenaventuraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuenaventuraBackendApplication.class, args);
+	}
+
+}
