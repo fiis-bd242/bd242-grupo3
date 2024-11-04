9a46f2220126697334e6e192fb3ccfaa9ad6f192
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2024-11-03 20:18:03 -0500

    Creacion del Frontend

---
+++ Aplicacion/Frontend/Buenaventura-Frontend/src/components/WelcomeItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="item">
+    <i>
+      <slot name="icon"></slot>
+    </i>
+    <div class="details">
+      <h3>
+        <slot name="heading"></slot>
+      </h3>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
---
+++ Aplicacion/Frontend/Buenaventura-Frontend/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1 class="text-7xl">Buenaventura</h1>
+    <h2 class="text-blue-600 h-1">Prueba</h2>
+  </main>
+</template>
