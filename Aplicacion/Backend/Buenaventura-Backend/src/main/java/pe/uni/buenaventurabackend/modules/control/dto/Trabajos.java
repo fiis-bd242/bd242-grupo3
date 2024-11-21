0b9a96878582f5c716c5d26aca4e46eefde8b092
unknown <lenin.quispe.g@uni.pe>
2024-11-20 19:58:10 -0500

    actu

---
+++ Aplicacion/Backend/Buenaventura-Backend/src/main/java/pe/uni/buenaventurabackend/modules/control/dto/Trabajos.java
@@ -0,0 +1,12 @@
+package pe.uni.buenaventurabackend.modules.control.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Trabajos {
+    private int
+}
