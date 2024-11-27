d02ba84f7f886fbed76f744b6262aa60bcf9373d
Adal1475 <adalito.14@gmail.com>
2024-11-27 16:26:14 -0500

    control

--- Aplicacion/Backend/Buenaventura-Backend/src/main/java/pe/uni/buenaventurabackend/modules/control/service/Notificacion1Service.java
+++ Aplicacion/Backend/Buenaventura-Backend/src/main/java/pe/uni/buenaventurabackend/modules/control/service/Notificacion1Service.java
@@ -3,6 +3,8 @@ package pe.uni.buenaventurabackend.modules.control.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pe.uni.buenaventurabackend.modules.control.models.Notificacion1;
+import pe.uni.buenaventurabackend.modules.control.repository.INotificacion1Repository;
 import pe.uni.buenaventurabackend.modules.planificacion.models.Notificaciones;
 import pe.uni.buenaventurabackend.modules.planificacion.repository.IPlanRepository;
 
@@ -11,24 +13,25 @@ import pe.uni.buenaventurabackend.modules.planificacion.repository.IPlanReposito
 @RequiredArgsConstructor
 public class Notificacion1Service implements INotificacion1Service {
     private final IPlanRepository iPlanRepository;
+    private final INotificacion1Repository iNotificacion1Repository;
 
     @Transactional
     @Override
     public void aceptarNotificacion(int id_usuario, int id_plan){
-        Notificaciones noti = new Notificaciones();
+        Notificacion1 noti = new Notificacion1();
         noti.setId_notificacion(iPlanRepository.conteoNotificaciones() +1);
         noti.setId_remitente(id_usuario);
         noti.setId_plan(id_plan);
-        iPlanRepository.envioNotificacion(noti);
+        iNotificacion1Repository.aceptarNotificacion(noti);
     }
 
     @Transactional
     @Override
     public void rechazarNotificacion(int id_usuario, int id_plan){
-        Notificaciones noti = new Notificaciones();
+        Notificacion1 noti = new Notificacion1();
         noti.setId_notificacion(iPlanRepository.conteoNotificaciones() +1);
         noti.setId_remitente(id_usuario);
         noti.setId_plan(id_plan);
-        iPlanRepository.envioNotificacion(noti);
+        iNotificacion1Repository.rechazarNotificacion(noti);
     }
 }
