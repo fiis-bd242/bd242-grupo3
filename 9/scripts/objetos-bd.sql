5af9c55e60a9ddba093fb435be4da8c6d52e9721
Adal1475 <adalito.14@gmail.com>
2024-11-30 14:53:40 -0500

    back

--- 9/scripts/objetos-bd.sql
+++ 9/scripts/objetos-bd.sql
@@ -1,24 +1,34 @@
 -- Índices
 --- Planificacion
+DROP INDEX IF EXISTS IDX_Maquina_mantto;
 CREATE INDEX IDX_Maquina_mantto ON Mantenimiento(id_maquina);
-CREATE INDEX IDX_Fecha_mantto ON Mantenimiento(fecha_inicio_programado)
+
+DROP INDEX IF EXISTS IDX_Fecha_mantto;
+CREATE INDEX IDX_Fecha_mantto ON Mantenimiento(fecha_inicio_programado);
 
 --- Control
 --- Equipos de soporte
 --- IPERC
 --- Insumos
+DROP INDEX IF EXISTS idx_reserva_fecha_hora;
 CREATE INDEX idx_reserva_fecha_hora ON Reserva(Fecha, Hora);
+
+DROP INDEX IF EXISTS idx_estado_reserva;
 CREATE INDEX idx_estado_reserva ON Estado_reserva(Id_estado_reserva);
 
 --- Seguridad
 --- Reportes
+DROP INDEX IF EXISTS idx_registros_fecha;
 CREATE INDEX idx_registros_fecha ON Registro(Fecha_inicial, fecha_registro);
+
+DROP INDEX IF EXISTS idx_reportes_fecha;
 CREATE INDEX idx_reportes_fecha ON reportes (fecha_reporte);
 
 
 
 -- Vistas
 --- Planificacion
+DROP VIEW IF EXISTS vista_equipos_soporte;
 CREATE VIEW vista_equipos_soporte AS
 SELECT 
     esm.id_act_mantto,
@@ -32,6 +42,7 @@ FROM EquipoSXMantenimiento esm
 INNER JOIN Equipo_de_Soporte es ON es.id_equipo_soporte = esm.id_equipo_soporte
 GROUP BY esm.id_act_mantto;
 
+DROP VIEW IF EXISTS vista_insumos;
 CREATE VIEW vista_insumos AS
 SELECT 
     im.id_act_mantto,
@@ -46,6 +57,7 @@ FROM InsumoXMantenimiento im
 INNER JOIN Insumo i ON i.id_insumo = im.id_insumo
 GROUP BY im.id_act_mantto;
 
+DROP VIEW IF EXISTS vista_plan_mantenimiento;
 CREATE VIEW vista_plan_mantenimiento AS
 SELECT 
     CONCAT('PL-', LPAD(p.id_plan::TEXT, 4, '0')) AS id_plan,
