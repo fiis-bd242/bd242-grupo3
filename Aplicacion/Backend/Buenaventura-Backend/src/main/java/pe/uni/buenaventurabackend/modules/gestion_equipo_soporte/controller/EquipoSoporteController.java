f81ce49356902f9612d4121f59acbb3b543b0805
Tilin1136 <saul.anyaipoma11@gmail.com>
2024-12-03 05:22:47 -0500

    correccion chevezzzzz

--- Aplicacion/Backend/Buenaventura-Backend/src/main/java/pe/uni/buenaventurabackend/modules/gestion_equipo_soporte/controller/EquipoSoporteController.java
+++ Aplicacion/Backend/Buenaventura-Backend/src/main/java/pe/uni/buenaventurabackend/modules/gestion_equipo_soporte/controller/EquipoSoporteController.java
@@ -1,5 +1,10 @@
 package pe.uni.buenaventurabackend.modules.gestion_equipo_soporte.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api/equipo_soporte")
 @RequiredArgsConstructor
