c4bb3a8d4aeda5f0997e960771025a7092c7706f
LeninQuispe25 <82728513+LeninQuispe25@users.noreply.github.com>
2024-12-04 14:06:01 -0500

    Update objetos-bd.sql

--- 9/scripts/objetos-bd.sql
+++ 9/scripts/objetos-bd.sql
@@ -107,6 +107,27 @@ LEFT JOIN vista_insumos insumos ON insumos.id_act_mantto = m.id_act_mantto
 WHERE act.nombre_actv = 'Responsable' AND m.id_estado != 8
 ORDER BY p.id_plan;
 
+--- Control del mantenimiento
+
+CREATE VIEW vista_trabajos_mantto AS
+	SELECT 
+        LPAD(p.id_plan::TEXT, 4, '0') AS id_plan,
+        CONCAT('MQ-', LPAD(m.id_maquina::TEXT, 4, '0')) AS id_maquina,
+        tm.nombre_tipo_mant,
+        c.nivel,
+        m.fecha_inicio_programado,
+        em.estado
+    FROM Plan_de_Mantenimiento p
+    INNER JOIN Mantenimiento m
+        ON m.id_plan = p.id_plan
+    INNER JOIN estado_mantto em
+        ON em.id_estado = m.id_estado
+    INNER JOIN Tipo_mantenimiento tm
+        ON tm.id_tipo_mant = m.id_tipo_mant
+    INNER JOIN Criticidad c
+        ON c.id_criticidad = p.id_criticidad
+    ORDER BY p.id_plan;
+
 --- Gestión de insumos
 
 DROP VIEW IF EXISTS vista_reservas;
@@ -114,7 +135,6 @@ CREATE VIEW vista_reservas AS
 SELECT id_reserva, fecha, hora, id_estado_reserva
 FROM reserva;
 
---- Control
 --- Equipos de soporte
 DROP VIEW IF EXISTS vista_detalle_equipo;
 CREATE VIEW vista_detalle_equipo AS
